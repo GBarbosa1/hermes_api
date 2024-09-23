84bd929cc392d0631f8db295a88a3dec12c450c4
GBarbosa1 <barbosa.gbsantos@gmail.com>
2024-09-23 20:44:06 -0300

    Removed unused endpoint

--- src/main.py
+++ src/main.py
@@ -9,8 +9,3 @@ stock = GetStock()
 async def get_symbol(symbol : str):
     stock.stock_fundamentals(symbol)
     return stock.payload
-    
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
