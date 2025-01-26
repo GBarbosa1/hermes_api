4174f8d55e8950abd746fa379af58a6dd18705c3
GBarbosa1 <barbosa.gbsantos@gmail.com>
2025-01-25 21:55:39 -0300

    Added endpoint to get prices with date filter

--- src/handlers/getsymbol.py
+++ src/handlers/getsymbol.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+from datetime import datetime, timedelta
 
 class getSymbol():
     
@@ -27,4 +28,22 @@ class getSymbol():
             "price_earnings":self.trailingPe,
             "dividend_yield":(self.div)*100
             }
+        }
+        
+    def stock_price(self, symbol, start_date, end_date = None):
+        start_date = datetime.strptime(start_date, "%Y-%m-%d").date()
+        if end_date is None:
+            end_date = start_date + timedelta(days=1)
+        else:
+            pass
+        stock = yf.Ticker(symbol)
+        self.payload = stock.history(start=start_date, end = end_date)
+        stock_row = self.payload.iloc[0]
+
+        self.payload = {
+            "open": stock_row["Open"],
+            "high": stock_row["High"],
+            "low": stock_row["Low"],
+            "close": stock_row["Close"],
+            "volume": int(stock_row["Volume"]),
         }
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from pydantic import BaseModel
 from fastapi import FastAPI
 from handlers.getsymbol import getSymbol
 from infraestructure.bcb_api import getBcbData
@@ -7,9 +8,18 @@ app = FastAPI()
 stock = getSymbol()
 bcb = getBcbData()
 
-@app.get("/stock/{symbol}")
+class DateSearch(BaseModel):
+    start_date: str
+    end_date: str = False
+
+@app.get("/stock/fundamentals/{symbol}")
 async def get_symbol_fundamentals(symbol : str):
     stock.stock_fundamentals(symbol)
+    return stock.payload
+
+@app.get("/stock/price/{symbol}")
+async def get_symbol_price(symbol : str, start_date:DateSearch):
+    stock.stock_price(symbol, start_date.start_date)
     return stock.payload
 
 @app.get("/actual_brazilian_interest_rate/")
